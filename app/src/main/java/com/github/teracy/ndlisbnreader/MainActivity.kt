789e13e7e49cc61921d1f982d0bd9857f39fe581
teracy <teracy.2006.10.06@gmail.com>
2019-04-23 11:22:42 +0900

    feat: Implement NDL Open Search API client

---
+++ app/src/main/java/com/github/teracy/ndlisbnreader/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.github.teracy.ndlisbnreader
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.github.teracy.ndlapi.OpenSearchApiClient
+import com.github.teracy.ndlisbnreader.util.AppSchedulerProvider
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import java.util.concurrent.TimeUnit
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val interceptor = HttpLoggingInterceptor()
+            .setLevel(HttpLoggingInterceptor.Level.BASIC)
+        val okHttpClient = OkHttpClient.Builder()
+            .connectTimeout(30, TimeUnit.SECONDS)
+            .readTimeout(30, TimeUnit.SECONDS)
+            .writeTimeout(30, TimeUnit.SECONDS)
+            .addInterceptor(interceptor)
+            .build()
+
+        val client = OpenSearchApiClient(okHttpClient)
+        val schedulerProvider = AppSchedulerProvider()
+
+        val subscribe = client.search("9784563005641")
+            .subscribeOn(schedulerProvider.newThread())
+            .observeOn(schedulerProvider.ui())
+            .subscribe({
+                System.out.printf("response:%d\n", it.items.size)
+            }, {
+                System.out.println("error:" + it.message)
+            })
+    }
+}
