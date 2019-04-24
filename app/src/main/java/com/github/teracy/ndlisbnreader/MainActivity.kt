e420e59dbbc7125b39c529b5ac729facdcc752d7
teracy <teracy.2006.10.06@gmail.com>
2019-04-24 14:52:35 +0900

    feat: Imprement openCamera method with PermissionsDispatcher

--- app/src/main/java/com/github/teracy/ndlisbnreader/MainActivity.kt
+++ app/src/main/java/com/github/teracy/ndlisbnreader/MainActivity.kt
@@ -1,8 +1,16 @@
 package com.github.teracy.ndlisbnreader
 
+import android.Manifest
+import android.content.Context
 import android.graphics.SurfaceTexture
+import android.hardware.camera2.CameraCaptureSession
+import android.hardware.camera2.CameraDevice
+import android.hardware.camera2.CameraManager
 import android.os.Bundle
+import android.support.annotation.StringRes
+import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
+import android.view.Surface
 import android.view.TextureView
 import android.view.View
 import android.view.ViewTreeObserver
@@ -11,9 +19,16 @@ import com.github.teracy.ndlisbnreader.util.AppSchedulerProvider
 import kotlinx.android.synthetic.main.activity_main.*
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
+import permissions.dispatcher.*
 import java.util.concurrent.TimeUnit
 
+@RuntimePermissions
 class MainActivity : AppCompatActivity() {
+    private var cameraDevice: CameraDevice? = null
+    private val cameraManager: CameraManager by lazy {
+        getSystemService(Context.CAMERA_SERVICE) as CameraManager
+    }
+    private var captureSession: CameraCaptureSession? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,7 +44,8 @@ class MainActivity : AppCompatActivity() {
                 override fun onSurfaceTextureDestroyed(p0: SurfaceTexture?): Boolean = true
 
                 override fun onSurfaceTextureAvailable(p0: SurfaceTexture?, p1: Int, p2: Int) {
-                    openCamera()
+                    // Annotation ProcessorによってopenCameraから生成されたメソッドを呼ぶ
+                    callCameraManagerWithPermissionCheck()
                 }
             }
         }
@@ -58,11 +74,87 @@ class MainActivity : AppCompatActivity() {
                 System.out.println("error:" + it.message)
             })
     }
-}
 
-private fun openCamera() {
-    System.out.println("openCamera called")
-    // TODO: Runtime Permissionとカメラ処理を実装する
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        // NOTE: 以下はAnnotation Processorで生成
+        onRequestPermissionsResult(requestCode, grantResults)
+    }
+
+    @NeedsPermission(Manifest.permission.CAMERA)
+    fun callCameraManager() {
+        if (cameraManager.cameraIdList.isEmpty()) {
+            System.out.println("!!!!!!!!!!!cameraIdList is empty!!!!!!!!!!!")
+            return
+        }
+        openCamera()
+    }
+
+    @Throws(SecurityException::class)
+    private fun openCamera() {
+        System.out.println("openCamera called")
+        cameraManager.openCamera(cameraManager.cameraIdList.get(0), object : CameraDevice.StateCallback() {
+            override fun onOpened(camera: CameraDevice) {
+                cameraDevice = camera
+                createCameraPreviewSession()
+            }
+
+            override fun onDisconnected(camera: CameraDevice) {
+                cameraDevice?.close()
+                cameraDevice = null
+            }
+
+            override fun onError(camera: CameraDevice, p1: Int) {
+                cameraDevice?.close()
+                cameraDevice = null
+            }
+        }, null)
+    }
+
+    private fun createCameraPreviewSession() {
+        if (cameraDevice == null) {
+            return
+        }
+        val texture = textureView.surfaceTexture
+        texture.setDefaultBufferSize(640, 480)
+        val surface = Surface(texture)
+
+        val previewRequestBuilder = cameraDevice!!.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW)
+        previewRequestBuilder.addTarget(surface)
+
+        cameraDevice?.createCaptureSession(listOf(surface), object : CameraCaptureSession.StateCallback() {
+            override fun onConfigured(session: CameraCaptureSession) {
+                captureSession = session
+                captureSession?.setRepeatingRequest(previewRequestBuilder.build(), null, null)
+            }
+
+            override fun onConfigureFailed(session: CameraCaptureSession) {}
+        }, null)
+    }
+
+    @OnShowRationale(Manifest.permission.CAMERA)
+    fun showRationaleForCamera(request: PermissionRequest) {
+        showRationaleDialog(R.string.permission_camera_rationale, request)
+    }
+
+    @OnPermissionDenied(Manifest.permission.CAMERA)
+    fun onCameraDenied() {
+//        Toast.makeText(this, R.string.permission_camera_denied, Toast.LENGTH_SHORT).show()
+    }
+
+    @OnNeverAskAgain(Manifest.permission.CAMERA)
+    fun onCameraNeverAskAgain() {
+//        Toast.makeText(this, R.string.permission_camera_never_askagain, Toast.LENGTH_SHORT).show()
+    }
+
+    private fun showRationaleDialog(@StringRes messageResId: Int, request: PermissionRequest) {
+        AlertDialog.Builder(this)
+            .setPositiveButton(android.R.string.ok) { _, _ -> request.proceed() }
+            .setNegativeButton(android.R.string.no) { _, _ -> request.cancel() }
+            .setCancelable(false)
+            .setMessage(messageResId)
+            .show()
+    }
 }
 
 /**
